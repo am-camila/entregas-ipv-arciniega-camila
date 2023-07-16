c0250db13f349f89146b8fbedcd27cfe90d29ccd
am-camila <marchinicamila94@gmail.com>
2023-07-16 17:18:16 -0300

    Entrega 6

--- entrega-6-arciniega-camila/template-inicial/src/entities/enemies/Turret.gd
+++ entrega-6-arciniega-camila/template-inicial/src/entities/enemies/Turret.gd
@@ -3,6 +3,9 @@ extends StaticBody2D
 onready var fire_position = $FirePosition
 onready var fire_timer = $FireTimer
 onready var raycast = $RayCast2D
+onready var sprite: Sprite = $Sprite
+onready var animation: AnimationPlayer = $AnimationPlayer
+
 
 export (PackedScene) var projectile_scene
 
@@ -17,6 +20,7 @@ func initialize(container, turret_pos, projectile_container):
 	container.add_child(self)
 	global_position = turret_pos
 	self.projectile_container = projectile_container
+	animation.play("Idle")
 
 func fire():
 	if target != null:
@@ -27,6 +31,7 @@ func fire():
 		fire_timer.start()
 
 func _physics_process(delta):
+	sprite.flip_h = target.global_position.x - global_position.x < 0
 	raycast.set_cast_to(raycast.to_local(target.global_position))
 	if raycast.is_colliding() && raycast.get_collider() == target:
 		if fire_timer.is_stopped():
@@ -37,8 +42,11 @@ func _physics_process(delta):
 
 func notify_hit():
 	print("I'm turret and imma die")
+	animation.play("dead")
+	fire_timer.stop()
+	yield(get_tree().create_timer(1.5),"timeout")
 	call_deferred("_remove")
-
+	
 func _remove():
 	get_parent().remove_child(self)
 	queue_free()
